8e6442ba257c8934f8cda9162701069b2748fc3f
Steven Maloney <steven.maloney@gmail.com>
2020-12-04 17:03:59 -0600

    styled it up a bit

--- src/components/GalleryItem/Galleryitem.js
+++ src/components/GalleryItem/Galleryitem.js
@@ -21,7 +21,7 @@ class GalleryItem extends Component{
             <div className="imageCard">
                 {/* <img onClick={(event)=> }src={this.props.image.path} className="imageDazzler" alt={this.props.image.description} /> */}
                     <div className="imageDazzler" onClick={(event)=> this.toggleImage(this.state.showImage)}>
-                        {this.state.showImage === true ? <img className="imageDazzler" src={this.props.image.path} alt={this.props.image.description}></img> :  <span>{this.props.image.description}</span>}                    
+                        {this.state.showImage === true ? <img className="imageDazzler" src={this.props.image.path} alt={this.props.image.description}></img> :  <span className="spanStyle">{this.props.image.description}</span>}                    
                     </div>
                     <button className="buttonStyle" onClick={(event) => this.props.addlike(this.props.image.id)}>Love it!</button>
                     <p>{this.props.image.likes === 0 ? `No people love this :(`:`${this.props.image.likes} people love this`}</p>
