0c5b3f4d3bf8c81d99565d856b1896d86f365d7b
Steven Maloney <steven.maloney@gmail.com>
2020-12-03 18:30:01 -0600

    get request works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
 
 class App extends Component {
+  state = {
+    imageList: []
+  }
+
+  getImages = () => {
+    axios.get('/gallery')
+    .then((response) => {
+      this.setState({
+        imageList: response.data
+      })
+    })
+    .catch((error) => {
+      alert('Something Bad Happened!!')
+      console.log('Error:', error);
+    })
+  }
+
+  componentDidMount(){
+  }
+
+
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br/>
-        <p>Gallery goes here</p>
-        <img src="images/aubrey.jpeg"/>
+        
+        <p>Gallery</p>
+        <GalleryList />
       </div>
     );
   }
---
+++ src/components/GalleryList/GalleryList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class GalleryList extends Component{
+
+    render(){
+        return(
+            <p>Hi from template!</p>
+        )
+    }
+}
+
+export default GalleryList;
