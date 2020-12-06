f096d0dfa12674eb83bf921aeeac856678c34ca4
Steven Maloney <steven.maloney@gmail.com>
2020-12-05 23:25:34 -0600

    styled up a bit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import '../App/App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Form extends Component{
 
     render(){
         return(
-            <form className="formBackground">
+            <form className="formBackground"> 
                 <h4>Add another image!</h4>
-                <label>URL</label>
-                <input type="text" onChange={this.props.handlechange('path')}/>
-                <label>Description</label>
-                <input type="text" onChange={this.props.handlechange('description')}/>
-                <button onClick={(event) => this.props.addimage(event)}>Submit</button>
+                <TextField id="outlined-basic" label="Description" variant="outlined" onChange={this.props.handlechange('path')}/>
+                <TextField id="outlined-basic" label="URL Path" variant="outlined" onChange={this.props.handlechange('description')}/>
+                <Button variant="outlined" color="primary" size="large" onClick={(event) => this.props.addimage(event)}>Submit</Button>
             </form>
         )
     }
--- src/components/GalleryItem/Galleryitem.js
+++ src/components/GalleryItem/Galleryitem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App/App.css';
+import Button from '@material-ui/core/Button';
 
 
 
@@ -23,8 +24,8 @@ class GalleryItem extends Component{
                     <div className="imageDazzler" onClick={(event)=> this.toggleImage(this.state.showImage)}>
                         {this.state.showImage === true ? <img className="imageDazzler" src={this.props.image.path} alt={this.props.image.description}></img> :  <span className="spanStyle">{this.props.image.description}</span>}                    
                     </div>
-                    <button className="buttonStyle" onClick={(event) => this.props.addlike(this.props.image.id, this.props.image.likes)}>Love it!</button>
-                    <button onClick={(event) => this.props.deleteimage(this.props.image.id)}>Delete</button>
+                    <Button variant="contained" color="primary" className="buttonStyle" onClick={(event) => this.props.addlike(this.props.image.id, this.props.image.likes)}>Love it!</Button>
+                    <Button variant="contained" color="secondary" onClick={(event) => this.props.deleteimage(this.props.image.id)}>Delete</Button>
                     <p>{this.props.image.likes === 0 ? `No people love this :(`:`${this.props.image.likes} people love this`}</p>
                     
             </div>
