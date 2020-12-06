d5080a515870c75f56992ab58e51cd3162055bdc
Steven Maloney <steven.maloney@gmail.com>
2020-12-05 22:49:14 -0600

    added white background for visibility

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../App/App.css';
 
 class Form extends Component{
 
     render(){
         return(
-            <form>
+            <form className="formBackground">
                 <h4>Add another image!</h4>
                 <label>URL</label>
                 <input type="text" onChange={this.props.handlechange('path')}/>
